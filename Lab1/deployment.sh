abd3f0b10a28692a43971834abb949c4dba26b86
Maksymilian Łazarski <01158804@pw.edu.pl>
2023-03-26 16:57:50 +0200

    Builds network, NSGs, bad public IP & no subnets

--- Lab1/deployment.sh
+++ Lab1/deployment.sh
@@ -14,15 +14,13 @@ az login
 CONFIG_FILE="$1"
 RESOURCE_GROUP_NAME="$(jq -r '.resource_group_name' "$CONFIG_FILE")"
 LOCATION="$(jq -r '.location' "$CONFIG_FILE")"
-NETWORK_ADDR_PREFIX="$(jq -r 'network.address_prefix' "$CONFIG_FILE")"
-NETWORK_NAME=="$(jq -r '.network.name' "$CONFIG_FILE")"
+# TODO these need to be different for every network
+NETWORK_ADDR_PREFIX="$(jq -r '.network.address_prefix' "$CONFIG_FILE")"
+NETWORK_NAME="$(jq -r '.network.name' "$CONFIG_FILE")"
 
 
-echo $RESOURCE_GROUP_NAME
-echo $LOCATION
-
-az group create --name \
-    --$RESOURCE_GROUP_NAME \
+az group create \
+    --name $RESOURCE_GROUP_NAME \
     --location $LOCATION
 
  
@@ -31,85 +29,90 @@ az network vnet create \
     --name $NETWORK_NAME \
     --address-prefix $NETWORK_ADDR_PREFIX
 
-#readarray -t NETWORK_SECURITY_GROUPS < <(jq -c ')
 readarray -t DEPLOYMENTS < <(jq -c '.deployments[]' "$CONFIG_FILE")
 
 for DEPLOYMENT in "${DEPLOYMENTS[@]}"; do   
-    echo $DEPLOYMENT
-
-    DEPLOYMENT_NAME=$(jq -r '.name' "$CONFIG_FILE")
-    # Network Security Groups
+    DEPLOYMENT_NAME=$(jq -r '.name' <<< "$DEPLOYMENT")
+    echo $DEPLOYMENT_NAME Network Security Groups creation
     NSG_NAME="${DEPLOYMENT_NAME}NSG"
     az network nsg create \
         --resource-group $RESOURCE_GROUP_NAME \
         --name $NSG_NAME 
 
     # Rules
-    #PORT=$(jq -c '.subnet.port' <<< "$CONFIG_FILE")
-    #SUBNET=$(jq -c '.subnet' <<< "$CONFIG_FILE")
+    #PORT=$(jq -c '.subnet.port' <<< "$DEPLOYMENT")
+    #SUBNET=$(jq -c '.subnet' <<< "$DEPLOYMENT")
 
-    #RULES=$(jq -c '.subnet.rules[]' <<< "$CONFIG_FILE")
+    #RULES=$(jq -c '.subnet.rules[]' <<< "$DEPLOYMENT")
     #for RULE in "${RULES}"; do
     #    echo $RULE
 
-    PORT=$(jq -c '.subnet.port' <<< "$CONFIG_FILE")    
+    PORT=$(jq -c '.subnet.port' <<< "$DEPLOYMENT")   
 
     az network nsg rule create \
         --resource-group $RESOURCE_GROUP_NAME \
-        --nsg-name $DEPLOYMENT \
-        --name "${DEPLOYMENT}Rule" \
+        --nsg-name $NSG_NAME \
+        --name "${DEPLOYMENT_NAME}Rule" \
         --access allow \
         --protocol Tcp \
         --priority 900 \
         --destination-port-ranges $PORT
         #TODO skipped unused values - may cause errors
 
-    # Subnet Creation
+    echo $DEPLOYMENT_NAME subnet creation
     SUBNET_NAME="${DEPLOYMENT_NAME}SubNet"
     az network vnet subnet create \
         --resource-group $RESOURCE_GROUP_NAME \
         --vnet-name $NETWORK_NAME \
         --name $SUBNET_NAME 
 
-    # Public ips
-    PUBLIC_IP=$(jq -c '.subnet.public_ip' <<< "$CONFIG_FILE")
+    echo $DEPLOYMENT_NAME Public ips creation
+    # TODO acquire real address and read it here. Also check if not empty
+    PUBLIC_IP=$(jq -r '.subnet.public_ip' <<< "$DEPLOYMENT")
 
     az network public-ip create \
         --resource-group $RESOURCE_GROUP_NAME \
         --name $PUBLIC_IP
-#done
 
-#VM Create
-VM_PRIVATE_IP_ADDRESS=$(jq -r 'virtual_machine.private_ip_address' <<< $DEPLOYMENT)
-VM_PUBLIC_IP_ADDRESS=$(jq -r 'virtual_machine..public_ip_address' <<< $DEPLOYMENT)
-VM_NAME="${DEPLOYMENT_NAME}VM"
-az vm create \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --vnet-name $NETWORK_NAME \
-    --name $VM_NAME \
-    --subnet $SUBNET_NAME \
-    --nsg "" \
-    --private-ip-address $VM_PRIVATE_IP_ADDRESS \
-    --public-ip-address $VM_PUBLIC_IP_ADDRESS \
-    --image UbuntuLTS \
-    --generate-ssh-keys
-    #TODO sizes
+    echo $DEPLOYMENT_NAME VM creation
+    VM_PRIVATE_IP_ADDRESS=$(jq -r '.virtual_machine.private_ip_address' <<< "$DEPLOYMENT")
+    VM_PUBLIC_IP_ADDRESS=$(jq -r '.virtual_machine.public_ip_address' <<< "$DEPLOYMENT")
+    VM_NAME="${DEPLOYMENT_NAME}VM"
+    az vm create \
+        --resource-group $RESOURCE_GROUP_NAME \
+        --vnet-name $NETWORK_NAME \
+        --name $VM_NAME \
+        --subnet $SUBNET_NAME \
+        --nsg "" \
+        --private-ip-address $VM_PRIVATE_IP_ADDRESS \
+        --public-ip-address $VM_PUBLIC_IP_ADDRESS \
+        --image UbuntuLTS \
+        --generate-ssh-keys
+        #TODO sizes and loop "vm create" over deployments for be/fe/db
 
-readarray -t DEPLOY < <(jq -c '.deploy[]' <<< $DEPLOYMENT)
+    #TODO uncomment once we know VMs are properly configured
+    # readarray -t DEPLOY < <(jq -c '.deploy[]' <<< $DEPLOYMENT)
+    # for SERVICE in "${DEPLOY[@]}"; do
+    #     case $DEPLOYMENT_NAME in
+    #         frontend)
+    #             echo Performing frontend setup...
 
-for SERVICE in "${DEPLOY[@]}"; do
-    echo $SERVICE
-
-    case $DEPLOYMENT_NAME in
-        frontend)
-            echo Frontend setup
-
-            az vm run-command invoke \
-                --resource-group $RESOURCE_GROUP_NAME \ 
-                --name $VM_NAME \
-                --commnad-id RunShellScript \
-                --scripts "@./front.sh" \
-;;
-
+    #             az vm run-command invoke \
+    #                 --resource-group $RESOURCE_GROUP_NAME \ 
+    #                 --name $VM_NAME \
+    #                 --scripts "@./deployment_fe.sh"
+    #                 # --command-id RunShellScript \
+    #     ;;
+    #         backend)
+    #             echo Performing backend setup...
+    #             az vm run-command invoke \
+    #                 --resource-group $RESOURCE_GROUP_NAME \ 
+    #                 --name $VM_NAME \
+    #                 --scripts "@./deployment_be.sh"
+    #     ;;
+    #         *)
+    #             echo Unknown setup $DEPLOYMENT_NAME
+    #     ;;
+    #     esac
+    # done
 done
-
--- Lab1/true.sh
+++
@@ -1,9 +0,0 @@
-
-CONFIG_FILE="$1"
-readarray -t SUBNETS < <(jq -c '.subnets[]' "$CONFIG_FILE")
-
-
-for SUBNET in "${SUBNETS[@]}"; do   
-    echo $SUBNET
-
-done
