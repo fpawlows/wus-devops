7e3bb48bdb81d0b6cf5a34ffdb67c0378822bb4b
fpawlows <filip.pawlowski001@gmail.com>
2023-04-18 21:44:13 +0200

    Perl script. Application properties

--- Lab1/deployment_be.sh
+++ Lab1/deployment_be.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+#TODO rewrite
+
+set -euxo pipefail
+
+if [ $# -lt 2 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  echo "Usage: $0 DATABASE_ADDRESS DATABASE_PORT DATABASE_USER DATABASE_PASSWORD"
+  exit 2 
+fi
+
+wiesz co w mysql properties tez jest w poetclinic
+
+DATABASE_ADDRESS=$1
+DATABASE_PORT=$2
+DATABASE_USER=$3
+DATABASE_PASSWORD=$4
+
 sudo apt-get update
 sudo apt-get upgrade -y
 
@@ -8,9 +26,13 @@ git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
 cd spring-petclinic-rest
 
 SPRING_PROP="./spring-petclinic-rest/src/main/resources/application.properties"
-
-
+MYSQL_PROP="./spring-petclinic-rest/src/main/resources/application-mysql.properties"
 
 sed -i "s/spring.profiles.active=hsqldb/spring.profiles.active=mysql/g" $SPRING_PROP
+sed -i "s/server.port=9966/server.port=$DATABASE_PORT/g" $SPRING_PROP
+
+sed -i "s/localhost:3306/$DATABASE_ADDRESS:$DATABASE_PORT/g" $MYSQL_PROP
+sed -i "s/spring.datasource.username=pc/spring.datasource.username=$DATABASE_USER/g" $MYSQL_PROP
+sed -i "s/spring.datasource.password=petclinic/spring.datasource.password=$DATABASE_PASSWORD/g" $MYSQL_PROP
 
 ./mvnw spring-boot:run
--- Lab1/deployment_db.sh
+++ Lab1/deployment_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "Correct usage: $0 DATABASE_USER DATABASE_PASSWORD "
     exit 1
 fi
@@ -18,8 +18,10 @@ sudo systemctl enable --now mysql
 wget $INIT_DB
 wget $POPULATE_DB
 
-sudo mysql -e "CREATE USER 'root_db'@'%' IDENTIFIED BY 'root_db';"
-sudo mysql -e "GRANT ALL ON *.* To 'root_db'@'%' WITH GRANT OPTION;"
+# sudo mysql -e "CREATE USER 'pc'@'%' IDENTIFIED BY 'pc';"
+# sudo mysql -e "GRANT ALL ON *.* To 'pc'@'%' WITH GRANT OPTION;"
+
+perl -0777 -i.original -pe 's/GRANT ALL PRIVILEGES ON petclinic.* TO pc\@localhost IDENTIFIED BY \x27pc\x27;/CREATE USER \x27pc\x27\@\x27localhost\x27 IDENTIFIED BY \x27pc\x27;\nGRANT ALL PRIVILEGES ON petclinic.* TO \x27pc\x27\@\x27localhost\x27;/igs' initDB.sql
 cat ./initDB.sql | sudo mysql -f
 cat ./populateDB.sql | sudo mysql -f
 
