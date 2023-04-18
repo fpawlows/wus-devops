8520704e7494c8967f27a905a43327901fb9ff68
fpawlows <filip.pawlowski001@gmail.com>
2023-04-18 14:11:11 +0200

    Frontend adjustment

--- Lab1/deployment_fe.sh
+++ Lab1/deployment_fe.sh
@@ -1,5 +1,6 @@
 if [ $# -ne 1 ]; then
     echo "Correct usage: $0 [port]"
+    exit 1
 fi
 
 PORT=$1
@@ -7,15 +8,17 @@ PORT=$1
 sudo apt-get update
 sudo apt-get upgrade -y
 
-curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt install git -y
 sudo apt install curl -y
 sudo apt install nodejs -y
 sudo apt install nginx -y
-# sudo apt install npm -y # why not????
+sudo apt install npm -y # why not????
 
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 
-# ...why??? https://github.com/nvm-sh/nvm/issues/2432
+#Necessary to run in one shell - source: https://github.com/nvm-sh/nvm/issues/2432
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" # This loads nvm bash_completion
