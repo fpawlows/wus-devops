70d1ce2aa82db3ac762d7af39232d4cf7ff111bf
Maksymilian Łazarski <01158804@pw.edu.pl>
2023-05-13 20:26:20 +0200

    Updated init

---
+++ Lab2/init_master.sh
@@ -0,0 +1,12 @@
+sudo apt update -y
+sudo apt upgrade -y
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+sudo apt install python3-pip
+
+python3 -m pip install ansible
+ansible-galaxy collection install azure.azcollection
+
+sudo pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
+
+az login
