27d3d4f566625c9658bbb9cd6313fc289d056a23
fpawlows <filip.pawlowski001@gmail.com>
2023-05-16 21:25:11 +0200

    Init_master.sh added installment of the requirements of azure

--- Lab2/init_master.sh
+++ Lab2/init_master.sh
@@ -6,6 +6,11 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo apt install python3-pip
 
 python3 -m pip install ansible
+
+curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+pip3 install -r requirements-azure.txt
+rm requirements-azure.txt
+
 ansible-galaxy collection install azure.azcollection
 
 sudo pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
