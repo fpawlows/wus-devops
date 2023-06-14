03f34785fd940377b8135529a97652e6984fa8fd
Maksymilian Łazarski <01158804@pw.edu.pl>
2023-06-14 14:21:03 +0200

    minor changes

--- Lab3/deploy.sh
+++ Lab3/deploy.sh
@@ -13,14 +13,17 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-az aks get-credentials \
-    --resource-group $1 \
-    --name $2
+RESOURCE_GROUP_NAME=$1
+CLUSTER_NAME=$2
 
+az aks get-credentials \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $CLUSTER_NAME
+
+kubectl get nodes
 
 cd spring-petclinic-cloud/
 kubectl apply -f k8s/init-namespace/
-kubectl create secret generic wavefront -n spring-petclinic --from-literal=wavefront-url=https://wavefront.surf --from-literal=wavefront-api-token=2e41f7cf-1111-2222-3333-7397a56113ca
 kubectl apply -f k8s/init-services
 
 
@@ -32,6 +35,8 @@ helm repo update
 helm install vets-db-mysql bitnami/mysql --namespace spring-petclinic --version 9.4.6 --set auth.database=service_instance_db
 helm install visits-db-mysql bitnami/mysql --namespace spring-petclinic  --version 9.4.6 --set auth.database=service_instance_db
 helm install customers-db-mysql bitnami/mysql --namespace spring-petclinic  --version 9.4.6 --set auth.database=service_instance_db
+
+export REPOSITORY_PREFIX=springcommunity
 
 ./scripts/deployToKubernetes.sh
 kubectl get svc -n spring-petclinic
